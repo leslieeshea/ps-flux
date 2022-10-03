99764b92ae657ff2a1dd2eae9aa4e36ad81fc8c7
leslieshea <lshea@audigy.com>
2022-10-03 13:05:49 -0700

    Added simple routing for pages

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+// this component will decide which page to render
+import React from 'react';
+import HomePage from './HomePage';
+import AboutPage from './AboutPage';
+
+function App() {
+	const route = window.location.pathname;
+
+	if (route === '/about') return <AboutPage />;
+
+	return <HomePage />;
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 // creates a const called 'render' that references react-dom's render function
 import { render } from 'react-dom';
-import HomePage from './components/HomePage';
-import AboutPage from './components/AboutPage';
+import App from './components/App';
 
 // first arg is the component that you want to render
 // second arg is the DOM element where you want to place your element
-render(<HomePage />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
