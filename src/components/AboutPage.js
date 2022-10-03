66b6e78577355ca68faefb15175743f0b4c538b7
leslieshea <lshea@audigy.com>
2022-10-03 12:48:24 -0700

    Created function component and class component

---
+++ src/components/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+	render() {
+		return (
+			// this is a React Fragment
+			<>
+				<h2>About</h2>
+				<p>This app uses React.</p>
+			</>
+		);
+	}
+}
+
+export default AboutPage;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+// creates a const called 'render' that references react-dom's render function
+import { render } from 'react-dom';
+import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
+
+// first arg is the component that you want to render
+// second arg is the DOM element where you want to place your element
+render(<HomePage />, document.getElementById('root'));
