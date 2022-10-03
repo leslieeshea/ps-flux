3f0f5f09ebb27697d269746c9a8ece78ea2414a4
leslieshea <lshea@audigy.com>
2022-10-03 16:14:07 -0700

    Added Router and Link to app

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import React from 'react';
 // creates a const called 'render' that references react-dom's render function
 import { render } from 'react-dom';
 import App from './components/App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 // first arg is the component that you want to render
 // second arg is the DOM element where you want to place your element
-render(<App />, document.getElementById('root'));
+render(
+	<Router>
+		<App />
+	</Router>,
+	document.getElementById('root')
+);
