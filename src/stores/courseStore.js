5394c106f45d03328cd9aaf2d012cf1de365337f
leslieshea <lshea@audigy.com>
2022-10-04 14:31:16 -0700

    Registered action to the dispatcher

--- src/stores/courseStore.js
+++ src/stores/courseStore.js
@@ -1,6 +1,9 @@
 import { EventEmitter } from 'events';
+import Dispatcher from '../appDispatcher';
+import actionTypes from '../actions/actionTypes';
 
 const CHANGE_EVENT = 'change';
+let _courses = [];
 
 class CourseStore extends EventEmitter {
 	// this will allow React components to subscribe to our store so they're notified when changes occur
@@ -16,8 +19,26 @@ class CourseStore extends EventEmitter {
 	emitChange() {
 		this.emit(CHANGE_EVENT);
 	}
+
+	getCourses() {
+		return _courses;
+	}
+
+	getCourseBySlug(slug) {
+		return _courses.find(course => course.slug === slug);
+	}
 }
 
 const store = new CourseStore();
+
+Dispatcher.register(action => {
+	switch(action.actionType) {
+		case actionTypes.CREATE_COURSE:
+			_courses.push(action.course);
+			store.emitChange();
+			break;
+		default:
+	}
+});
 
 export default store;
