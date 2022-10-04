c5a0e16e9354a5e8239e92db2f0bd3879e013695
leslieshea <lshea@audigy.com>
2022-10-04 14:51:24 -0700

    Can update a course with Flux store

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
 	CREATE_COURSE: 'CREATE_COURSE',
-	LOAD_COURSES: 'LOAD_COURSES'
+	LOAD_COURSES: 'LOAD_COURSES',
+	UPDATE_COURSE: 'UPDATE_COURSE'
 };
