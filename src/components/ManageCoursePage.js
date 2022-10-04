741d6b3b91e0d5e32c06bf094acdd9b042f01193
leslieshea <lshea@audigy.com>
2022-10-03 17:39:33 -0700

    Added change handler to title input

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CourseForm from './CourseForm';
 
 const ManageCoursePage = props => {
+	const [course, setCourse] = useState({
+		id: null,
+		slug: '',
+		title: '',
+		authorId: null,
+		category: ''
+	});
+
+	function handleTitleChange(event) {
+		//copy the course object and set the title property to value passed in by the event
+		const updatedCourse = {...course, title: event.target.value};
+		setCourse(updatedCourse);
+	}
+
 	return (
 		<>
 			<h2>Manage Course</h2>
-			{props.match.params.slug}
+			<CourseForm course={course} onTitleChange={handleTitleChange} />
 		</>
 	);
 }
